bceb572c87cc2d4150461ca005f214cb73952a0d
NITHISH0403 <nithish.kumar@tringapps.com>
2023-02-13 14:32:02 +0530

    Initial commit

---
+++ src/main/java/org/example/Frequency.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.logging.*;
+
+public class Frequency {
+    public static void main(String[] args) throws Exception {
+        HashMap<String, Integer> map = new HashMap<>();
+        String[] str;
+        String line;
+        StringBuilder Text = new StringBuilder(" ");
+        Logger l = Logger.getLogger("NITHISH");
+
+        //Open File in read mode
+        FileReader file = new FileReader("C:\\Users\\Tringapps-user16\\OneDrive\\Documents\\TextFile.txt");
+        BufferedReader br = new BufferedReader(file);
+
+        //Reads each line
+        while ((line = br.readLine()) != null) {
+            Text.append(line);
+        }
+        str = Text.toString().split(" ");
+        for(int i = 0 ; i<str.length; i++) {
+            String val = str[i];
+            l.log(Level.INFO,()->val);
+            //this method returns true if some keys equal to the key exits
+            if (map.containsKey(str[i])) {
+                map.put(str[i], map.get(str[i]) + 1);
+            }
+            else{
+                map.put(str[i],1);
+            }
+        }
+        l.info("Sorted Text....");
+        PriorityQueue<Map.Entry<String, Integer>> p = new PriorityQueue<>((a,b)-> b.getValue() - a.getValue());
+        p.addAll(map.entrySet());
+        while (!p.isEmpty()){
+            l.log(Level.INFO,()-> String.valueOf(p.poll()));
+        }
+    }
+}
